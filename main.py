ddbd5265b1860abf32e069a1632f5452a4301099
Renan Fritsch <bfritschrenan@gmail.com>
2026-01-15 14:11:27 -0300

    feat: configurar inicializacao da aplicacao

---
+++ main.py
@@ -0,0 +1,17 @@
+# from src.core.servicos import GerenciadorDespesa
+# from src.interface.gui.tela_inicial import TelaInicial
+
+# despesa1 = Despesa('feijão', 150, '2026-01-30', Status('paga'), Tipo('unica'))
+# print(despesa1)
+
+
+# resultado = GerenciadorDespesa().criar_despesa('frango', 150, '2026-01-30', 'paga', 'unica')
+# print(resultado.mensagem)
+
+
+import flet as ft
+
+def main(page: ft.Page):
+    pass
+
+ft.app(target=main)
