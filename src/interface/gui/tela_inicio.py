a162c71e23e1c2cf5118a4c4346251dc8d723ea2
Renan Fritsch <bfritschrenan@gmail.com>
2026-01-22 14:55:29 -0300

    refactor: Refatoração do codigo organização e documentação.

--- src/interface/gui/tela_inicio.py
+++ src/interface/gui/tela_inicio.py
@@ -6,21 +6,25 @@ class TelaInicial(ft.Container):
         self.expand = True
         self.constraints=ft.BoxConstraints(min_width=350, max_width=600),
         self.alignment = ft.Alignment.CENTER
+
         self.content = ft.ListView(
             expand=True, # Ocupa o espaço disponível no container
-            divider_thickness=10, # Adiciona uma linha divisória visual entre os itens com a espessura definida.
             spacing=10, # Define o espaço em pixels entre cada item da lista.
             padding=20, #Espaçamento interno entre a borda da lista e os itens.
             width=900, # Aqui você define a largura máxima pros cards!
             item_extent=110, # Altura fixa para scroll liso
             cache_extent= 600,
 
-            controls=[]  
+            controls=[]  # Lista responsavel por agrupar o conteudo da tela.
         )
-        self.lista = ["1","2","3","4","5",]
-        self.cards = [self.card_despesa(nome) for nome in self.lista]
-        for card in self.cards: 
-            self.content.controls.append(card)
+        self.content.controls.append(self.cabecalho()) # Adiciona o titúlo na tela
+
+        self.lista = ["1","2","3","4","5",] # lista para testes
+        self.cards = [self.card_despesa(nome) for nome in self.lista] # Cria a lista de cards para lançarem na tela
+        self.content.controls.extend(self.cards) # ADD os cards na lista do ListView para ser lançado na tela
+
+        # for card in self.cards: 
+        #     self.content.controls.append(card) # ADD os cards na lista do ListView para ser lançado na tela
 
     def mudar(self):
         self.content.controls.append(self.card_despesa("Teste"))
@@ -47,7 +51,7 @@ class TelaInicial(ft.Container):
         # g = self.content.controls
         # print(self.g)
         return ft.ShaderMask(
-                    border_radius=ft.border_radius.all(15),
+                    border_radius=ft.BorderRadius.all(15),
                     blend_mode=ft.BlendMode.MODULATE,
                     shader=ft.LinearGradient(
                         colors=[ft.Colors.RED_400, ft.Colors.WHITE], # Muda as cores do gradiente
